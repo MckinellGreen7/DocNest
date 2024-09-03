27ad89cfc4ac8098de653c36569188d91edf35a0
MckinellGreen7 <khandelwalshriyansh0412@gmail.com>
2024-09-03 12:32:08 +0530

    second commit

--- backend/models/useModel.js
+++ backend/models/useModel.js
@@ -1,6 +1,8 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
 mongoose.connect(process.env.MONGODB_URI);
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
