3e510d1bd70ab1a289a60248e10c6fc546d7123a
MckinellGreen7 <khandelwalshriyansh0412@gmail.com>
2024-09-03 12:20:10 +0530

    first commit

---
+++ backend/models/docModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI);
+
+const docSchema = mongoose.Schema({
+  title: String,
+  content: {
+    type: String,
+    default: ""
+  },
+  uploadedBy: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  lastUpdate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Document', docSchema);
---
+++ backend/models/useModel.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI);
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    phone: String,
+    username: String,
+    isBlocked: {
+      type: Boolean,
+      default: false
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
